c6b7feca2ee8a300ab96a8b1a56270ffef3ce58b
CyberShur <aldrobakha@gmail.com>
2025-11-03 21:24:17 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+# Дробаха Александр, 37_qa_plus — дипломный проект
+URL_SERVICE = "https://4bfd482c-cf78-4dd4-b996-fbbef94a376c.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,12 @@
+# Дробаха Александр, 37_qa_plus — дипломный проект
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Иванов",
+    "address": "Москва, ул. Седова, 15",
+    "metroStation": 4,
+    "phone": "+79111111111",
+    "rentTime": 5,
+    "deliveryDate": "2025-11-05",
+    "comment": "Тест тест тест",
+    "color": ["BLACK"]
+}
