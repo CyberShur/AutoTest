f11943c571bc3fd6abc44c90fff394c9c02994d6
CyberShur <aldrobakha@gmail.com>
2025-11-04 12:01:08 +0300

    Добавлен файл автотеста и исправлен формат в соответствии с PEP8

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 # Дробаха Александр, 37_qa_plus — дипломный проект
-URL_SERVICE = "https://4bfd482c-cf78-4dd4-b996-fbbef94a376c.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://1210cf1f-d6b5-49ee-bdff-60dd5224e697.serverhub.praktikum-services.ru"
 CREAT_ORDERS = "/api/v1/orders"
